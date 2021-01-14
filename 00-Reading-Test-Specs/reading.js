a5089af99d366d7ec4c08ac38aa40b139f52ecba
Jonathan Chiang <77289486+jmchiang1@users.noreply.github.com>
2021-01-13 23:43:22 -0500

    Update reading.js

--- 00-Reading-Test-Specs/reading.js
+++ 00-Reading-Test-Specs/reading.js
@@ -1,11 +1,13 @@
 function hello(name){
-	if(typeof name === "undefined"){
+	if(typeof name === "undefined"){		// if the value type is undefined, return "Hello!"
 		return "Hello!";
-	} else {
-		return "Hello, " + name + "!";
+	} else {					// otherwise,
+		return "Hello, " + name + "!";		// return ...
 	}
 }
 
 function add(number, num){
 	return number + num;
 }
+
+console.log(name);
